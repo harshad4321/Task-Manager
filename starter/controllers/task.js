ff006349c6a5e9eb02928bef5184915ad02f9424
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-06-21 21:13:56 +0530

    middleware add for handling in the error

--- starter/controllers/task.js
+++ starter/controllers/task.js
@@ -1,58 +1,53 @@
 const task = require('../models/task')
 const Task = require('../models/task')
+const asyncWrapper = require('../middleware/async')
 
-const getAllTasks = async(req,res)=>{
-  try{
+// grt all task
+
+const getAllTasks =asyncWrapper( async(req,res)=>{
     const  tasks = await Task.find({})
-    res.status(200).json({tasks,amount:tasks.length})
-  } catch(error){
-    res.status(500).json({msg:error})
+    res.status(200).json({tasks})
+  
+})
 
-  }
-}
+//create task
 
+const createTask = asyncWrapper(async(req,res)=>{
 
-const createTask = async(req,res)=>{
-    try{
         const task = await Task.create(req.body)
         res.status(201).json({task})
 
-    }catch(error){
-        res.status(500).json({msg:error})
-    }
-   
-}
-const getTask = async(req,res)=>{
-    try{
+})
+
+// get task
+
+const getTask = asyncWrapper(async(req,res)=>{
         const{id:taskID}= req.params
         const task = await Task.findOne({_id:taskID});
         if(!task){
             return res.status(404).json({msg:`no task with id:${taskID}`})
         }
         res.status (200). json({task})
-    }catch(error){
-        res.status(500).json({msg:error})
-    }
   
-}
+})
 
+//delete task
 
-const deleteTask = async(req,res)=>{ 
-    try{
+const deleteTask =asyncWrapper( async(req,res)=>{ 
+  
         const{id:taskID}= req.params;
         const task = await Task.findOneAndDelete({_id:taskID});
         if(!task){
             return res.status(404).json({msg:`no task with id:${taskID}`})
         }
         res.status (200). json({task})
-    }catch(error){
-        res.status(500).json({msg:error})
-    }
-}
+   
+})
 
+//update task
 
-const updateTask = async(req,res)=>{  
-    try{
+const updateTask = asyncWrapper(async(req,res)=>{  
+  
         const{id:taskID}= req.params;
 const task = await task.findOneAndUpdate({_id:taskID},req.body,{
     new:true,
@@ -62,11 +57,8 @@ if(!task){
     return res.status(404).json({msg:`no task with id:${taskID}`})
 }
 res.status (200). json({task})
-
-    }catch(error){
-        res.status(500).json({msg:error})
-    }
-}
+   
+})
 
 
 module.exports={
